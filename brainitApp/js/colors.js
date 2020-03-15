d8bbee2e518c6b9a2af2b3707e3b7a0e3fb901f3
David Ruiz <davidl.ruizc@gmail.com>
2020-03-15 14:57:08 -0500

    login component

---
+++ brainitApp/js/colors.js
@@ -0,0 +1,8 @@
+export const primaryColor = "#851A1A";
+
+export const navBarColor = "#789DD0";
+export const navIconColor = "#BFCEE5";
+
+export const bodyColorHome = "#5787C9";
+export const bodyColor1 = "#B5C9E5";
+export const bodyColor2 = "#A1BDE5";
--- brainitApp/js/screens/Login.js
+++ brainitApp/js/screens/Login.js
@@ -1,21 +1,68 @@
-import React from 'react';
-import { View, Text, StyleSheet,  } from 'react-native';
-import { Input } from 'react-native-elements';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { Input, Button } from 'react-native-elements';
 
+import Layout from '../components/Auth/Layout';
+import { primaryColor } from '../colors';
 
 const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleOnPressLogin = ({ navigation }) => {
+    console.warn('Logged in');
+  };
+
+  const handleCreateAccountNavigation = () => {
+    // navigation.navigate()
+  };
+
   return(
-    <View style={styles.container}>
-      <Text style={styles.title}>WELCOME</Text>
-      <Input
-        placeholder='Email'
-        style={styles.input}
-        inputContainerStyle={styles.inputContainer}
-        keyboardType="email-address"
-        onChangeText={email => setEmail(email)}
-        labelStyle={styles.padding}
-      />
-    </View>
+    <Layout>
+      <View style={styles.container}>
+        <Text style={styles.title}>WELCOME</Text>
+        <Input
+          placeholder='Email'
+          style={styles.input}
+          inputContainerStyle={styles.inputContainer}
+          keyboardType="email-address"
+          onChangeText={email => setEmail(email)}
+          labelStyle={styles.padding}
+        />
+        <Input
+          placeholder="Password"
+          style={styles.input}
+          inputContainerStyle={styles.inputContainer}
+          onChangeText={password => setPassword(password)}
+          labelStyle={styles.padding}
+          secureTextEntry={true}
+        />
+        <Button
+          containerStyle={{
+            marginTop: 25,
+            marginBottom: 15,
+            width: '95%',
+            alignSelf: 'center',
+          }}
+          title="Sign in"
+          onPress={handleOnPressLogin}
+          buttonStyle={styles.button}
+          titleStyle={styles.buttonText}
+          disabled={email == '' || password == ''}
+        />
+        <TouchableOpacity
+          style={styles.wrapperForgot}
+        >
+          <Text style={styles.textLabel}>Forgot your password?</Text>
+        </TouchableOpacity>
+        <View style={styles.wrapper}>
+          <Text style={styles.text}>Don't have an account?</Text>
+          <TouchableOpacity>
+            <Text style={styles.textCreate}>Create new account</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </Layout>
   );
 };
 
@@ -24,25 +71,61 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    marginHorizontal: 15
   },
   title: {
-    fontSize: 30
+    fontSize: 30,
+    color: '#ffffff',
+    marginBottom: 30
   },
   padding: {
     marginTop: 20,
   },
   inputContainer: {
     borderColor: 'gray',
+    backgroundColor: '#fff',
     borderWidth: 1,
-    borderRadius: 30,
-    padding: 5,
-    paddingLeft: 20,
+    borderRadius: 10,
+    paddingLeft: 10,
     marginTop: 10,
   },
   input: {
     fontSize: 17,
     paddingTop: 40,
   },
+  button: {
+    backgroundColor: primaryColor,
+    borderRadius: 10,
+    borderColor: 'transparent',
+    borderWidth: 1.4,
+  },
+  buttonText: {
+    paddingTop: 6,
+    paddingBottom: 6,
+    color: '#ffffff',
+    fontSize: 17,
+  },
+  textLabel: {
+    fontWeight: 'bold',
+    color: '#ffffff',
+    textAlign: 'right'
+  },
+  text: {
+    fontWeight: 'bold',
+    color: '#ffffff',
+    alignSelf: 'center'
+  },
+  textCreate: {
+    fontWeight: 'bold',
+    color: primaryColor,
+    alignSelf: 'center'
+  },
+  wrapperForgot: {
+    flexDirection: 'row'
+  },
+  wrapper: {
+    marginTop: 50
+  }
 });
 
 export default Login;
