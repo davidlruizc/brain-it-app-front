01c2d1f4fa8b82ee17113d7d91312c6a2d53d103
David Ruiz <davidl.ruizc@gmail.com>
2020-05-10 15:30:38 -0500

    adding card and image to grammar

---
+++ brainitApp/js/components/Grammar/Card.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import {View, Text, StyleSheet, ImageBackground, TouchableOpacity, } from 'react-native';
+
+const image = require('../../../assets/grammar-one.jpg');
+
+const Card = () => (
+  <TouchableOpacity style={styles.container}>
+    <ImageBackground
+      source={image}
+      style={styles.image}
+      imageStyle={{borderRadius: 10, backgroundColor: 'rgba(0,0,0,.6)'}}
+    >
+      <Text style={styles.title}>Simple Past</Text>
+    </ImageBackground>
+  </TouchableOpacity>
+);
+
+const styles = StyleSheet.create({
+  container: {
+    alignSelf: 'center',
+    marginTop: 19
+  },
+  image: {
+    width: 344,
+    height: 154,
+    justifyContent: 'flex-end',
+    padding: 19,
+  },
+  title: {
+    fontWeight: 'bold',
+    fontSize: 24,
+    color: 'white',
+  }
+})
+
+export default Card;
--- brainitApp/js/screens/Grammar/Grammar.js
+++ brainitApp/js/screens/Grammar/Grammar.js
@@ -1,14 +1,31 @@
 import React from 'react';
-import { View, Text } from 'react-native';
- 
+import { View, Text, StyleSheet, ScrollView } from 'react-native';
+
+import Layout from '../../components/Global/Layout';
+import Card from '../../components/Grammar/Card';
+
 const Grammar = () => {
   return(
-    <View>
-      <Text>
-        WELCOME Grammar
-      </Text>
-    </View>
+    <Layout>
+      <ScrollView style={styles.container} showsVerticalScrollIndicator={false} >
+        <Text style={styles.title}>Grammar</Text>
+        <Card
+        />
+      </ScrollView>
+    </Layout>
   );
-}
+};
+
+const styles = StyleSheet.create({
+  container: {
+    paddingHorizontal: 30,
+    marginTop: 30
+  },
+  title: {
+    fontSize: 36,
+    fontWeight: 'bold',
+    color: 'white'
+  }
+});
 
 export default Grammar;
