771d3f04203074a994d65d4bfaccd66a6f448d87
David Ruiz <davidl.ruizc@gmail.com>
2020-06-15 18:56:38 -0500

    update home content

--- brainitApp/js/screens/Home/Home.js
+++ brainitApp/js/screens/Home/Home.js
@@ -7,26 +7,26 @@ import VieoCard from '../../components/Home/VideoCard';
 const tempData = [
   {
     title: 'Welcome to brainit',
-    shortDescription: 'Short description of the video to give an intro for the audience',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    shortDescription: 'Welcome to the new app BrainIt. In this video, our hosts invite you to enjoy it.',
+    description: 'In these lessons, we are going to work on holidays and vacations. These activities are designed for students at an intermediate level. Then, you will have to do some activities in order to develop skills like reading and listening. You will also practice aspects such as grammar and vocabulary.',
     videoId: 'PAaJtiIG6ro'
   },
   {
     title: 'Simple past',
-    shortDescription: 'Short description of the video to give an intro for the audience',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    shortDescription: 'In this lesson, you will learn how the past simple is used in English.',
+    description: 'In this video, you will see the structure of the past simple tense (affirmative, negative, and interrogative forms), the difference between regular and irregular verbs, past time expressions, and some examples.',
     videoId: 'U9GfQpK9YPg'
   },
   {
     title: 'Past continuous',
-    shortDescription: 'Short description of the video to give an intro for the audience',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    shortDescription: 'In this lesson, you will learn from our hosts Alexandra and Lucia how the past progressive is used in English.',
+    description: 'In this video, you will see the structure (affirmative, negative and interrogative forms), the use of the past tense of To Be as an auxiliary, examples, and key words (while and when), in order to learn how to use it. With the examples given on the video it will be easy to understand it.',
     videoId: 'zQqZe6M64Yo'
   },
   {
     title: 'Past perfect',
-    shortDescription: 'Short description of the video to give an intro for the audience',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    shortDescription: 'In this lesson, you will see Alexandra and Lucia explaining how the past perfect is used in English.',
+    description: 'In this video, you will see the use of  past perfect tense and some examples. You are going to see two sentences in a timeline, and with them you will notice the order in which they happen. Also, you will find some key words and the use of the past tense of the verb Have as an auxiliary of the sentence.',
     videoId: 'KiW6dI3Mm5M'
   },
 ];
