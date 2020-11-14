36316b5aacbbf330e532b74b470d971f53a17ed6
David Ruiz <davidl-ruizc@outlook.com>
2020-11-14 15:31:38 -0500

    refactor

--- index.js
+++
@@ -1,8 +0,0 @@
-import { AppRegistry } from 'react-native';
-import 'react-native-gesture-handler';
-import App from './App.js';
-
-AppRegistry.registerComponent('brainitApp', () => App);
-
-// The below line is necessary for use with the TestBed App
-AppRegistry.registerComponent('ViroSample', () => App);
--- ios/Pods/Target Support Files/Pods-brainitApp/Pods-brainitApp-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_brainitApp : NSObject
-@end
-@implementation PodsDummy_Pods_brainitApp
-@end
