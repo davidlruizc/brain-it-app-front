05b7251f0e4efaa959e6e27c93f88eb03767178d
David Ruiz <davidl-ruizc@outlook.com>
2021-04-16 09:39:17 -0500

    use local images

--- brainItApp/src/components/Grammar/Card.js
+++ brainItApp/src/components/Grammar/Card.js
@@ -1,32 +1,44 @@
 import React from 'react';
-import {View, Text, StyleSheet, ImageBackground, TouchableOpacity, } from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  ImageBackground,
+  TouchableOpacity,
+} from 'react-native';
 
-// const image = require('../../../assets/grammar-one.jpg');
+const imageOne = require('../../../assets/grammar-one.jpg');
+const imageTwo = require('../../../assets/grammar-two.jpg');
+const imageThree = require('../../../assets/grammar-three.jpg');
 
-const Card = ({
-  title,
-  image,
-  text,
-  textTitle,
-  quiz,
-  handleNavigateToText,
-  uri
-}) => (
-  <TouchableOpacity style={styles.container} onPress={() => handleNavigateToText(textTitle, image, text, quiz)}>
-    <ImageBackground
-      source={uri ? {uri: image} : image}
-      style={styles.image}
-      imageStyle={{borderRadius: 10, backgroundColor: 'rgba(0,0,0,.6)'}}
-    >
-      <Text style={styles.title}>{title}</Text>
-    </ImageBackground>
-  </TouchableOpacity>
-);
+const Card = ({title, image, text, textTitle, quiz, handleNavigateToText}) => {
+  const imageSelected = React.useMemo(() => {
+    if (image === 'grammar-one') {
+      return imageOne;
+    } else if (image === 'grammar-two') {
+      return imageTwo;
+    } else {
+      return imageThree;
+    }
+  }, [image]);
+  return (
+    <TouchableOpacity
+      style={styles.container}
+      onPress={() => handleNavigateToText(textTitle, image, text, quiz)}>
+      <ImageBackground
+        source={imageSelected}
+        style={styles.image}
+        imageStyle={{borderRadius: 10, backgroundColor: 'rgba(0,0,0,.6)'}}>
+        <Text style={styles.title}>{title}</Text>
+      </ImageBackground>
+    </TouchableOpacity>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
     alignSelf: 'center',
-    marginTop: 19
+    marginTop: 19,
   },
   image: {
     width: 344,
@@ -38,7 +50,7 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     fontSize: 24,
     color: 'white',
-  }
-})
+  },
+});
 
 export default Card;
