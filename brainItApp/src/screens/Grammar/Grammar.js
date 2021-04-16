a542b097bbccfe36974f9eb8dc346a531840b922
David Ruiz <davidl-ruizc@outlook.com>
2021-04-16 09:31:55 -0500

    change resources

--- brainItApp/src/screens/Grammar/Grammar.js
+++ brainItApp/src/screens/Grammar/Grammar.js
@@ -8,7 +8,7 @@ const tempData = [
   {
     title: 'Past Simple',
     textTitle: 'A Victorious Flight',
-    image: 'https://brain-it-global.s3.amazonaws.com/grammar-one.jpg',
+    image: 'https://www.mediafire.com/convkey/1a95/36o9m3lxp7tdqb7zg.jpg',
     text: `
     My sister started studying Medicine at UBA in Argentina, so I decided to study there too. We hadn’t visited Colombia since January, that is the reason why our grandparents invited us to spend Colombian’s Independence Day with them in Bogotá. When we arrived at the airport, we were running because we were late.
     \n
@@ -48,7 +48,7 @@ const tempData = [
   {
     title: 'Past Progressive',
     textTitle: 'An unusual visit',
-    image: 'https://brain-it-global.s3.amazonaws.com/grammar-two.jpg',
+    image: 'https://www.mediafire.com/convkey/aab0/u6558n2hlw688zjzg.jpg',
     text: `
     Last Christmas I went to Acacías with my friends to spend holidays there. When we were reaching the bus terminal station, my friend Ana was buying our tickets. We arrived in Acacías that evening. While Mike and I were getting meat for our stay, Ana and Paul were buying some beers and groceries for the party. The next day, we decided to do a BBQ in the garden. While Ana and I were swimming at the pool, Mike and Paul were grilling the meat. We were having such a great time, and out of nowhere Paul’s mother arrived. As Paul was enjoying the BBQ party, he invited her to stay, but Ana, Mike and I were not comfortable with that. So while Paul was convincing his mother to go back home, we were cleaning the mess we made.
     \n
@@ -79,7 +79,7 @@ const tempData = [
   {
     title: 'Past Perfect',
     textTitle: `“An Amazonic Decision”`,
-    image: 'https://brain-it-global.s3.amazonaws.com/grammar-three.jpg',
+    image: 'https://www.mediafire.com/convkey/66b0/kt69sxmtpvdx85ezg.jpg',
     text: `
     When Sophy turned 30, she started to feel like everything she had done in the past was wrong. She had been calling her ex-boyfriend to tell him that she had missed him very much and she had thought to travel with him because she had been feeling they were meant to be together forever, but he was already dating another girl. It made her go on a trip to Amazonas. By the time she decided to quit her job, she had already sold her apartment. She had collected all the money she needed for her trip since she had been ready for this her entire life. 
     She had met Ethan, an Amazonas’ habitant by tinder, and he had invited her to move in together. By the time she had arrived at Amazonas they had had the idea to open a Pink Dolphin Sanctuary for tourists. She has started to fall in love with Ethan but she hasn’t told him yet.
