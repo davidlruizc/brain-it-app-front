5228e8f3400e2bfbb6e0241f802c19db6ef563f2
David Ruiz <davidl-ruizc@outlook.com>
2021-04-29 13:47:59 -0500

    changing pctures

--- brainItApp/src/components/Home/VideoCard.js
+++ brainItApp/src/components/Home/VideoCard.js
@@ -3,22 +3,47 @@ import {View, Text, StyleSheet, Image, TouchableOpacity} from 'react-native';
 
 import {secondaryColor} from '../../colors';
 
-const VideoCard = ({title, description, shortDescription, handleNavigation, videoId}) => (
-  <TouchableOpacity style={styles.wrapper} onPress={() => handleNavigation({title, description, videoId})} >
-    <View>
-      <Image
-        source={require('../../../assets/tempThumbnail.jpeg')}
-        style={styles.thumbnail}
-      />
-    </View>
-    <View style={styles.textWrapper}>
-      <Text style={styles.title}>{title}</Text>
-      <View style={{flexDirection: 'row'}}>
-        <Text style={styles.description}>{shortDescription}</Text>
+const thumbOne = require('../../../assets/tempThumbnail.jpeg');
+const thumbTwo = require('../../../assets/thumbnailPast.png');
+const thumbThree = require('../../../assets/thumbnailContinuous.png');
+const thumbFour = require('../../../assets/thumbnailPerfect.png');
+
+const VideoCard = ({
+  title,
+  description,
+  shortDescription,
+  handleNavigation,
+  videoId,
+  image,
+}) => {
+  const imageSelected = React.useMemo(() => {
+    if (image === 'tempThumbnail') {
+      return thumbOne;
+    } else if (image === 'thumbnailContinuous') {
+      return thumbThree;
+    } else if (image === 'thumbnailPast') {
+      return thumbTwo;
+    } else if (image === 'thumbnailPerfect') {
+      return thumbFour;
+    }
+  }, [image]);
+
+  return (
+    <TouchableOpacity
+      style={styles.wrapper}
+      onPress={() => handleNavigation({title, description, videoId})}>
+      <View>
+        <Image source={imageSelected} style={styles.thumbnail} />
       </View>
-    </View>
-  </TouchableOpacity>
-);
+      <View style={styles.textWrapper}>
+        <Text style={styles.title}>{title}</Text>
+        <View style={{flexDirection: 'row'}}>
+          <Text style={styles.description}>{shortDescription}</Text>
+        </View>
+      </View>
+    </TouchableOpacity>
+  );
+};
 
 const styles = StyleSheet.create({
   wrapper: {
@@ -26,27 +51,27 @@ const styles = StyleSheet.create({
     padding: 17,
     backgroundColor: secondaryColor,
     borderRadius: 10,
-    marginTop: 19
+    marginTop: 19,
   },
   thumbnail: {
     width: 156,
     height: 86,
-    borderRadius: 10
+    borderRadius: 10,
   },
-  textWrapper: {
+  textWrapper: {
     marginHorizontal: 7,
   },
   title: {
     fontSize: 14,
     fontWeight: 'bold',
-    color: 'white'
+    color: 'white',
   },
   description: {
     flex: 1,
     fontSize: 11,
     color: 'white',
-    flexWrap: 'wrap'
-  }
-})
+    flexWrap: 'wrap',
+  },
+});
 
 export default VideoCard;
--- brainItApp/src/screens/Home/Home.js
+++ brainItApp/src/screens/Home/Home.js
@@ -12,6 +12,7 @@ const tempData = [
     description:
       'In these lessons, we are going to work on holidays and vacations. These activities are designed for students at an intermediate level. Then, you will have to do some activities in order to develop skills like reading and listening. You will also practice aspects such as grammar and vocabulary.',
     videoId: 'PAaJtiIG6ro',
+    image: 'tempThumbnail',
   },
   {
     title: 'Simple past',
@@ -20,6 +21,7 @@ const tempData = [
     description:
       'In this video, you will see the structure of the past simple tense (affirmative, negative, and interrogative forms), the difference between regular and irregular verbs, past time expressions, and some examples.',
     videoId: 'U9GfQpK9YPg',
+    image: 'thumbnailPast',
   },
   {
     title: 'Past continuous',
@@ -28,6 +30,7 @@ const tempData = [
     description:
       'In this video, you will see the structure (affirmative, negative and interrogative forms), the use of the past tense of To Be as an auxiliary, examples, and key words (while and when), in order to learn how to use it. With the examples given on the video it will be easy to understand it.',
     videoId: 'zQqZe6M64Yo',
+    image: 'thumbnailContinuous',
   },
   {
     title: 'Past perfect',
@@ -36,6 +39,7 @@ const tempData = [
     description:
       'In this video, you will see the use of  past perfect tense and some examples. You are going to see two sentences in a timeline, and with them you will notice the order in which they happen. Also, you will find some key words and the use of the past tense of the verb Have as an auxiliary of the sentence.',
     videoId: 'KiW6dI3Mm5M',
+    image: 'thumbnailPerfect',
   },
 ];
 
